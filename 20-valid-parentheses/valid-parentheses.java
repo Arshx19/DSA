fa9ae93df103078d1fdb9682e5d86a5fc8deccb5
Arsh <arshjain2605@gmail.com>
2026-05-18 05:22:51 +0530

    Time: 3 ms (87.88%) | Memory: 43.5 MB (25.58%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,29 +1,19 @@
-import java.util.Stack;
-
 class Solution {
-    public boolean isValid(String str) {
+    public boolean isValid(String s) {
         Stack<Character> st = new Stack<>();
-
-        for (int i = 0; i < str.length(); i++) {
-            char curr = str.charAt(i);
-
-            if (curr == '(' || curr == '{' || curr == '[') {
-                st.push(curr);
-            } else {
-                if (st.isEmpty()) {
+        for(char ch : s.toCharArray()){
+            if(ch =='(' || ch=='{' || ch=='['){
+                st.push(ch);
+            }else{
+                if(st.isEmpty()){
                     return false;
                 }
-
-                char ch = st.pop();
-
-                if ((curr == ')' && ch == '(') || (curr == ']' && ch == '[') ||           (curr == '}' && ch == '{')) {
-                    continue;
-                } else {
-                    return false;
-                }
+            char top = st.pop();
+            if(ch==')' && top!='(') return false;
+            if(ch=='}' && top!='{') return false;
+            if(ch==']' && top!='[') return false;
             }
         }
-
         return st.isEmpty();
     }
 }
